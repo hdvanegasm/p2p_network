a68b24304080462117588ebd1d159f42b16cfdbd
hernan232 <hdvanegasm@gmail.com>
2020-01-12 19:15:32 -0500

    Server implementation

--- network.py
+++ network.py
@@ -1,3 +1,79 @@
 '''
 This is a basic implementation of a client for a P2P network.
 '''
+
+import socket
+import sys
+
+class Client(object):
+    pass
+
+class Server(object):
+
+    def __init__(self, byte_size):
+        try:
+            # List with connections to the server
+            self.connections = []
+
+            # List of peers connected
+            self.peers = []
+
+            # Socket instantiation and setup
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # Bind socket to local host
+            self.socket.bind(('127.0.0.1', 5000))
+
+            self.socket.listen(1)
+
+            self.byte_size = byte_size
+
+            print('==> Server running.')
+
+        except Exception as exception:
+            print(exception)
+            sys.exit()
+
+
+    def handler(self, connection_handler, ip_port_tuple):
+        try:
+            while True:
+                data = connection_handler.recv(self.byte_size)
+
+                # Check if the peer wants to disconnect
+                for connection in self.connections:
+                    if data and data.decode('utf-8') == 'q':
+                        self.disconnect(connection, ip_port_tuple)
+                        return
+                    else:
+                        connection.send(data.decode('utf-8'))
+        except Exception as exception:
+            print(exception)
+            sys.exit()
+
+
+    def disconnect(self, connection, ip_port_tuple):
+        self.connections.remove(connection)
+        self.peers.remove(ip_port_tuple)
+        connection.close()
+        self.send_peers()
+        print('NOTIFICATION: {} disconnected.'.format(ip_port_tuple))
+
+
+    def send_peers(self):
+        peer_list = ""
+        for peer in self.peers:
+            peer_list += str(peer[0]) + ','
+
+        for connection in self.connections:
+            connection.send('\x11' + bytes(peer_list, 'utf-8'))
+
+
+
+
+
+
+
+
+
