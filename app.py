e6173168549ae34d3505a4c17d917314550223ad
hernan232 <hdvanegasm@gmail.com>
2020-01-13 21:11:09 -0500

    App implementation. We must solve some errors.

---
+++ app.py
@@ -0,0 +1,33 @@
+from network import *
+
+def app():
+    while True:
+        try:
+            for peer in P2PNetwork.peers:
+                try:
+                    client = Client(peer)
+                    while True:
+                        message = input()
+                        if message == 'cmd_show_peers':
+                            client.send_message('cmd_show_peers')
+                        else:
+                            client.send_message(message)
+                except KeyboardInterrupt:
+                    sys.exit(0)
+                except Exception as exception:
+                    print(exception)
+
+                try:
+                    server = Server(byte_size = 1024)
+                    server.run()
+                except KeyboardInterrupt:
+                    sys.exit(0)
+                except Exception as exception:
+                    print(exception)
+
+
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+if __name__ == '__main__':
+    app()
--- network.py
+++ network.py
@@ -4,6 +4,11 @@ This is a basic implementation of a client for a P2P network.
 
 import socket
 import sys
+import threading
+
+class P2PNetwork(object):
+
+    peers = ['127.0.0.1']
 
 class Client(object):
 
@@ -14,7 +19,53 @@ class Client(object):
 
         # Make the connection
         self.socket.connect((address, 5000))
-        
+        self.bytesize = 1024
+
+        print('==> Connected to server.')
+
+        client_listener_thread = threading.Thread(target = self.client_listener)
+        client_listener_thread.start()
+
+    def send_message(self, message):
+        try:
+            self.socket.send(message.encode('utf-8'))
+        except KeyboardInterrupt:
+            self.send_disconnect_signal()
+        except Exception as exception:
+            print(exception)
+            self.send_disconnect_signal()
+
+    def receive_message(self):
+        try:
+            data = self.socket.recv(self.bytesize)
+            return data.decode('utf-8')
+        except KeyboardInterrupt:
+            self.send_disconnect_signal()
+
+    def send_disconnect_signal(self):
+        print('==> Disconected from server.')
+        self.socket.send("q".encode('utf-8'))
+        sys.exit()
+
+    def update_peers(self, peers):
+        P2PNetwork.peers = str(peers, 'utf-8').split(',')[:-1]
+
+    def client_listener(self):
+        while True:
+            try:
+                data = self.receive_message()
+                if not data:
+                    print("==> Server disconnected.")
+                    break
+                elif data[0:1] == b'\x11':
+                    print('==> Got peers.')
+                    self.update_peers(data[1:])
+                else:
+                    print("[#] " + data)
+            except ConnectionError:
+                print("==> Server disconnected.")
+                break
+
 
 class Server(object):
 
@@ -38,36 +89,33 @@ class Server(object):
             self.byte_size = byte_size
 
             print('==> Server running.')
-
         except Exception as exception:
             print(exception)
             sys.exit()
-
 
     def handler(self, connection_handler, ip_port_tuple):
         try:
             while True:
                 data = connection_handler.recv(self.byte_size)
-
                 # Check if the peer wants to disconnect
                 for connection in self.connections:
                     if data and data.decode('utf-8') == 'q':
                         self.disconnect(connection, ip_port_tuple)
                         return
-                    else:
-                        connection.send(data.decode('utf-8'))
+                    elif data.decode('utf-8') == 'cmd_show_peers':
+                        connection.send(('---' + str(self.peers)).encode('utf-8'))
+                    elif data:
+                        connection.send(data)
         except Exception as exception:
             print(exception)
             sys.exit()
-
 
     def disconnect(self, connection, ip_port_tuple):
         self.connections.remove(connection)
         self.peers.remove(ip_port_tuple)
         connection.close()
         self.send_peers()
-        print('NOTIFICATION: {} disconnected.'.format(ip_port_tuple))
-
+        print('{} disconnected.'.format(ip_port_tuple))
 
     def send_peers(self):
         peer_list = ""
@@ -75,4 +123,23 @@ class Server(object):
             peer_list += str(peer[0]) + ','
 
         for connection in self.connections:
-            connection.send('\x11' + bytes(peer_list, 'utf-8'))
+            connection.send(bytes('\x11' + peer_list, 'utf-8'))
+
+        print('==> Peers sent.')
+
+    def run(self):
+        # Listen to new connections
+        while True:
+            connection_handler, ip_port_tuple = self.socket.accept()
+
+            # Add the new peer and send to clients the new list
+            self.peers.append(ip_port_tuple)
+            self.send_peers()
+            self.connections.append(connection_handler)
+
+            # Initialize the handler thread
+            handler_thread = threading.Thread(target = self.handler, args = (connection_handler, ip_port_tuple,))
+            handler_thread.daemon = True
+            handler_thread.start()
+
+            print('==> {} connected.'.format(ip_port_tuple))
